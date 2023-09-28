3c467d9c55e14b6432e34dca75dd8aed6b0a2825
Facundo <facundo.troitero@fedrax.com>
2023-09-28 17:18:36 +0100

    first commit

---
+++ _scripts/main.js
@@ -0,0 +1,50 @@
+$(function() {
+  const d = new Date();
+  const hours = d.getHours();
+  const night = hours >= 19 || hours <= 7; // between 7pm and 7am
+  const body = document.querySelector('body');
+  const toggle = document.getElementById('toggle');
+
+  const prefersDarkMode = () =>
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  // if (prefersDarkMode() || night) {
+  //   body.classList.add('night');
+  // }
+
+  const introHeight = document.querySelector('.intro').offsetHeight;
+  const topButton = document.getElementById('top-button');
+  const $topButton = $('#top-button');
+
+  window.addEventListener(
+    'scroll',
+    function() {
+      if (window.scrollY > introHeight) {
+        $topButton.fadeIn(800);
+      } else {
+        $topButton.fadeOut(800);
+      }
+    },
+    false
+  );
+
+  topButton.addEventListener('click', function() {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+  });
+
+  window.sr = ScrollReveal({
+    reset: false,
+    duration: 800,
+    origin: 'bottom',
+    distance: '3%',
+    opacity: 0,
+    easing: 'cubic-bezier(.694,0,.335,1)',
+    viewFactor: 0.3,
+  });
+  sr.reveal('.intro', { distance: 0 });
+  sr.reveal('.background');
+  sr.reveal('.team');
+  sr.reveal('.experience', { viewFactor: 0.2 });
+  sr.reveal('.project', { viewFactor: 0.1 });
+});
